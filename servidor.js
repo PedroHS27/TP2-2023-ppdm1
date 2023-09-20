7fc82f58f1a68d07431b88e7443ea870b3fa51db
diegovnc <diego.vinicius@gsuite.iff.edu.br>
2023-09-20 13:26:55 -0300

    pão

--- servidor.js
+++ servidor.js
@@ -3,80 +3,37 @@ const express=require('express')
 const app=express()
 
 const fs=require('fs')
-
-
-app.use(express.urlencoded({extended:false}))
-
 app.use(express.json())
 
 
-app.set('view engine','ejs')
-
-app.get('/', (req,res)=>{
-
-res.render("Servidor")
-
-})
-
 app.post('/sucess',(req,res)=>{
-
-var email=req.body.Email
-
-fs.writeFileSync(email+".json",JSON.stringify(req.body),function(error){
-
-if(error){
-
-console.log("Coe, algo deu errado",erro);
-
- }
-
- })
-
-res.render('sucess')
-
- 
-
-
+var email=req.body.email
+fs.writeFileSync(email+".json",JSON.stringify(req.body))
+res.send('sucess')
 })
 
-app.post('/list',(req,res)=>{
-
+app.get('/list',(req,res)=>{
 fs.readdir(__dirname,(err,files)=>{ 
-
 var newFiles=files.filter(data=>data.includes('.json')) 
-
-console.log(newFiles);
-
-res.render('list', {dados:newFiles})
-
+res.send({dados:newFiles})
  })
-
 })
 
 
 app.get('/:email', (req,res)=>{
-
 var conta=req.params
-
 var dadosfinais=JSON.parse(fs.readFileSync(conta.email))
-
 res.send(dadosfinais)
-
 })
 
-app.get('/delete/:email',(req,res)=>{
-
+app.delete('/delete/:email',(req,res)=>{
 var dados=req.params.email
-
 fs.unlinkSync(dados)
-
 res.send("dados apagados com sucesso")
-
  })
 
+ app.use((req, res, next) => {
+    res.send({erro: true, msg: "Rota não definida no servidor."})
+});
 
-app.listen(8080, () =>{
-
-console.log("O servidor está rodando em localhost:8080");
-
-})
+app.listen(8080, () => console.log("O servidor está rodando em localhost:8080"))
